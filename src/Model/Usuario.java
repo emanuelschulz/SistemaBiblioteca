78d7ee40b3c06481e813ec3a6c75155f03118d02
rafaelPerez1 <rpeerez18@gmail.com>
2019-06-14 21:25:08 -0300

    ajustes classes biblioteca

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author gabriel
- */
-public class Usuario {}
+
+public class Usuario {
+private int Id_autor;
+private String nome;
+private String email;
+
+}
